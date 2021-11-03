6598f428e5fb1e91b161c998d6ec22e0430012fe
Gleb Gorelov <g.gorelov@haulmont.com>
2021-11-03 12:31:32 +0400

    UI Advanced Day 3

---
+++ src/main/resources/VAADIN/quill/quill-connector.js
@@ -0,0 +1,22 @@
+com_company_demo_QuillTextEditor = function () {
+    let connector = this;
+    let element = connector.getElement();
+    element.innerHTML = "<div id=\"editor\"></div>";
+
+    connector.onStateChange = function () {
+        let state = connector.getState();
+        let options = state.data;
+
+        var quill = new Quill('#editor', options);
+
+        quill.on('text-change', function (delta, oldDelta, source) {
+            if (source === 'user') {
+                connector.valueChanged(quill.getText());
+            }
+        });
+
+        connector.deleteText = function () {
+            quill.deleteText(0, quill.getLength());
+        }
+    }
+}
